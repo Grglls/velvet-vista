615d7db68601c5cc18b705767a7109103f3f7fe6
Greg Ells <gregells@gmail.com>
2025-04-01 17:51:05 -0400

    Re-format Routes in App page, add route for HomePage component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import NavBar from '../../components/NavBar/NavBar';
+import HomePage from '../HomePage/HomePage'
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
@@ -10,18 +11,16 @@ export default function App() {
   const [user, setUser] = useState(getUser());
 
   return (
-    <main className="App">
-      { user ?
-        <>
-          <NavBar user={user} setUser={setUser} />
-          <Routes>
-            <Route path="/orders/new" element={ <NewOrderPage /> } />
-            <Route path="/orders" element={ <OrderHistoryPage /> } />
-          </Routes>
-        </>
-          :
-        <AuthPage setUser={setUser} />
-      }
+    <main className="">
+      <NavBar user={user} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/orders" element={ <OrderHistoryPage /> } />
+        <Route path="/orders/new" element={ <NewOrderPage /> } />
+        <Route path="/login" element={<AuthPage setUser={setUser} />} />
+        {/* redirect to "/" if path in address bar hasn't matched a <Route> above */}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </main>
   )
 }
