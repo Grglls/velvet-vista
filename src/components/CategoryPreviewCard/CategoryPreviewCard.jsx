a3be2b0011047df17fc8ea13ccee92cfdd3fa731
Greg Ells <gregells@gmail.com>
2025-04-01 20:09:53 -0400

    Add CategoryPreviewCard component

---
+++ src/components/CategoryPreviewCard/CategoryPreviewCard.jsx
@@ -0,0 +1,10 @@
+export default function CategoryPreviewCard({ item }) {
+  return (
+    <div className="card p-0">
+      <img src={item.image} className="card-img-top" alt="..." />
+      <div className="card-body p-1">
+        <p className="card-text">{item.name}</p>
+      </div>
+    </div>
+  )
+}
