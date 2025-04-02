e69725688213b8fae88cc4e8fc9ce34a7ed3852f
Greg Ells <gregells@gmail.com>
2025-04-01 20:07:37 -0400

    Revise index controller action to sort items and populate category field

--- controllers/api/items.cjs
+++ controllers/api/items.cjs
@@ -3,16 +3,10 @@ const Item = require('../../models/item.cjs');
 module.exports = {
   index,
 };
-
+  
 async function index(req, res) {
-  try {
-    // Get all items from the database:
-    const items = await Item.find({});
-    console.log(items);
-    // Use res.json to send back just a string:
-    // (the client code needs to take this into consideration)
-    res.json(items);
-  } catch (error) {
-    res.status(400).json(error);
-  }
+  const items = await Item.find({}).sort('name').populate('category').exec();
+  // re-sort based upon the sortOrder of the populated categories
+  items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+  res.json(items);
 }
