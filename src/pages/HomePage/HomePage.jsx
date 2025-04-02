1bc93db2345a6d3c37a4ce1ef51ce54a26de3b1c
Greg Ells <gregells@gmail.com>
2025-04-01 20:09:18 -0400

    Render a CategoryPreview component for each category

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,18 @@
-export default function HomePage() {
+import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
+
+export default function HomePage({ clothesItems, categories }) {
+  console.log(categories);
+  const clothesCats = categories.map(cat => 
+    <CategoryPreview
+      category={cat} 
+      clothes={clothesItems.filter(item => item.category.name === cat)} 
+    />
+  );
+  
   return (
     <div className="container-fluid">
-      <h1>HomePage</h1>
-
+      <h1>Home</h1>
+      {clothesCats}
     </div>
   );
 }
