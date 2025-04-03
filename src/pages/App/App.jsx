568057cb62b53363cc209c3bc9a2cf54ed5a8af0
Greg Ells <gregells@gmail.com>
2025-04-03 18:06:37 -0400

    Add Routes for clothes categories

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -7,6 +7,7 @@ import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import * as itemsAPI from '../../utilities/items-api';
+import CategoryPage from '../CategoryPage/CategoryPage';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
@@ -25,14 +26,17 @@ export default function App() {
   return (
     <main className="">
       <NavBar user={user} setUser={setUser} categories={categoriesRef.current} />
-      <Routes>
-        <Route path="/" element={ <HomePage clothesItems={clothesItems} categories={categoriesRef.current} /> } />
-        <Route path="/orders" element={ <OrderHistoryPage /> } />
-        <Route path="/orders/new" element={ <NewOrderPage /> } />
-        <Route path="/login" element={<AuthPage setUser={setUser} />} />
-        {/* redirect to "/" if path in address bar hasn't matched a <Route> above */}
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
+      <div className="container-fluid mt-2" style={{"max-width": "1000px"}}>
+        <Routes>
+          <Route path="/" element={ <HomePage clothesItems={clothesItems} categories={categoriesRef.current} /> } />
+          <Route path="/orders" element={ <OrderHistoryPage /> } />
+          <Route path="/orders/new" element={ <NewOrderPage /> } />
+          <Route path="/login" element={<AuthPage setUser={setUser} />} />
+          <Route path="/category/:categoryId" element={<CategoryPage clothesItems={clothesItems} />} />
+          {/* redirect to "/" if path in address bar hasn't matched a <Route> above */}
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </main>
   )
 }
