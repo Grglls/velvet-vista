5001ee59dbdb420b588f6b975747f7a2131bb203
Greg Ells <gregells@gmail.com>
2025-04-03 18:08:29 -0400

    Revise naming scheme for clothing category links in CategoryPreview, update import for ClothingItemCard

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import CategoryPreviewCard from "../CategoryPreviewCard/CategoryPreviewCard";
+import ClothingItemCard from "../ClothingItemCard/ClothingItemCard";
 
 export default function CategoryPreview({ category, clothes }) {
   return (
     <div className="mb-3">
-      <Link to={`/products/${category}`} className="dropdown-item" ><h5 className="text-decoration-underline">{category}</h5></Link>
+      <Link to={`/category/${category}`} className="dropdown-item" ><h5 className="text-decoration-underline">{category}</h5></Link>
       <div className="row row-cols-3 p-2">
-        {clothes.map((item, index) => index < 3 && <CategoryPreviewCard item={item} key={index} /> )}
+        {clothes.map((item, index) => index < 3 && <ClothingItemCard item={item} key={index} /> )}
       </div>
     </div>
   );
