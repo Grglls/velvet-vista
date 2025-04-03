8d64e8b86fe99f7c6afd73d57f5d1dba8baa022a
Greg Ells <gregells@gmail.com>
2025-04-03 17:11:19 -0400

    Adjust card padding

--- src/components/CategoryPreviewCard/CategoryPreviewCard.jsx
+++ src/components/CategoryPreviewCard/CategoryPreviewCard.jsx
@@ -1,9 +1,11 @@
 export default function CategoryPreviewCard({ item }) {
   return (
-    <div className="card p-0">
-      <img src={item.image} className="card-img-top" alt="..." />
-      <div className="card-body p-1">
-        <p className="card-text">{item.name}</p>
+    <div className="p-1">
+      <div className="card p-0">
+        <img src={item.image} className="card-img-top" alt="..." />
+        <div className="card-body p-1">
+          <p className="card-text">{item.name}</p>
+        </div>
       </div>
     </div>
   )
