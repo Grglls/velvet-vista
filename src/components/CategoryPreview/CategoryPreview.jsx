8319aafee352eb05aa00cfec7ded930ad97c34ec
Greg Ells <gregells@gmail.com>
2025-04-04 13:25:49 -0400

    Adjust styling of CategoryPreview links to categories

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -4,7 +4,9 @@ import ClothingItemCard from "../ClothingItemCard/ClothingItemCard";
 export default function CategoryPreview({ category, clothes }) {
   return (
     <div className="mb-3">
-      <Link to={`/category/${category}`} className="dropdown-item" ><h5 className="text-decoration-underline">{category}</h5></Link>
+      <Link to={`/category/${category}`} className="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" >
+        <h5 className="text-decoration-underline d-inline-block">{category}:</h5>
+      </Link>
       <div className="row row-cols-3 p-2">
         {clothes.map((item, index) => index < 3 && <ClothingItemCard item={item} key={index} /> )}
       </div>
