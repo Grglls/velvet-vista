c1c0211b8bc1d4d2599789b413ce8b273a350023
Greg Ells <gregells@gmail.com>
2025-04-04 12:37:55 -0400

    Make cards in ClothingItemCard component Links to that items detail page

--- src/components/ClothingItemCard/ClothingItemCard.jsx
+++ src/components/ClothingItemCard/ClothingItemCard.jsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function ClothingItemCard({ item }) {
+  console.log(item);
   return (
     <div className="p-1">
-      <div className="card p-0">
+      <Link to={`/items/${item._id}`} className="card p-0" >
         <img src={item.image} className="card-img-top" alt="..." />
         <div className="card-body p-1">
           <p className="card-text">{item.name}</p>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
