cac811d90b9d72e1680b747454d38980ffd05a04
Greg Ells <gregells@gmail.com>
2025-04-04 15:14:10 -0400

    Fix error where reloading page from ItemDetailPage didn't work

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -2,7 +2,14 @@ import { useParams } from 'react-router-dom';
 
 export default function ItemDetailPage({ clothesItems }) {
   const itemId = useParams().itemId;
+
+  // Wait until clothesItems is populated:
+  if (!clothesItems.length) return <p>Loading...</p>;
+
   const item = clothesItems.find(item => item._id === itemId);
+
+  // Handle case where item is not found:
+  if (!item) return <p>Item not found.</p>;
 
   return (
     <>
