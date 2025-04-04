d8e6792e8732983232488bb2138592e79922e20a
Greg Ells <gregells@gmail.com>
2025-04-04 12:52:03 -0400

    Make cards in ClothingItemCard underlined on hover

--- src/components/ClothingItemCard/ClothingItemCard.jsx
+++ src/components/ClothingItemCard/ClothingItemCard.jsx
@@ -4,7 +4,10 @@ export default function ClothingItemCard({ item }) {
   console.log(item);
   return (
     <div className="p-1">
-      <Link to={`/items/${item._id}`} className="card p-0" >
+      <Link
+        to={`/items/${item._id}`}
+        className="card p-0 link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-offset-2"
+      >
         <img src={item.image} className="card-img-top" alt="..." />
         <div className="card-body p-1">
           <p className="card-text">{item.name}</p>
