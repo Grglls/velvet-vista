e85db5b3bbc36dc4dbf0b08e8fe45d47e06d82d9
Greg Ells <gregells@gmail.com>
2025-04-04 16:10:41 -0400

    Display ItemDetailPage as one column on mobile and 2 columns on small screens and larger

--- src/pages/ItemDetailPage/ItemDetailPage.jsx
+++ src/pages/ItemDetailPage/ItemDetailPage.jsx
@@ -14,12 +14,11 @@ export default function ItemDetailPage({ clothesItems }) {
   return (
     <>
       <h1>{item.name}</h1>
-      <div className="row row-cols-3 p-2">
-        <div className="col">
+      <div className="row row-cols-1 row-cols-sm-2 p-2">
+        <div className="mb-3">
           <img src={item.image} className="card-img-top" alt={item.name} />
         </div>
-        <div className="col">
-          <p>{item.description}</p>
+        <div className="mb-3">
           <p>Price: ${item.price}</p>
           <button className="btn btn-primary">Add to Cart</button>
         </div>
