4fb188bf4c158113d6f5da481efbecc63425b500
Greg Ells <gregells@gmail.com>
2025-04-04 13:26:41 -0400

    Add focus-ring, change underline colour of item links on ClothingItemCard component

--- src/components/ClothingItemCard/ClothingItemCard.jsx
+++ src/components/ClothingItemCard/ClothingItemCard.jsx
@@ -6,7 +6,7 @@ export default function ClothingItemCard({ item }) {
     <div className="p-1">
       <Link
         to={`/items/${item._id}`}
-        className="card p-0 link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-offset-2"
+        className="card h-100 p-0 focus-ring focus-ring-light link-light link-underline link-underline-opacity-0 link-underline-opacity-100-hover link-offset-2"
       >
         <img src={item.image} className="card-img-top" alt="..." />
         <div className="card-body p-1">
