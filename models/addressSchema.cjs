6af259a58a0c66e76d8a8b8e871ceb07e4cdded1
Greg Ells <gregells@gmail.com>
2025-04-05 16:06:54 -0400

    Add addressSchema module

---
+++ models/addressSchema.cjs
@@ -0,0 +1,16 @@
+const Schema = require('mongoose').Schema;
+
+const addressSchema = new Schema({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zip: { type: String, required: true },
+  country: { type: String, required: true },
+  phone: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+}, {
+  timestamps: true
+});
+
+module.exports = addressSchema;
