47e3cfc0c215a82a9365321d6241042c8913ec9c
Greg Ells <gregells@gmail.com>
2025-04-09 18:39:28 -0400

    Add controller action for adding item to cart

--- controllers/api/orders.cjs
+++ controllers/api/orders.cjs
@@ -1,10 +1,18 @@
+const { add } = require('../../models/itemSchema.cjs');
 const Order = require('../../models/order.cjs');
 
 module.exports = {
   cart,
+  addToCart,
 };
   
 async function cart(req, res) {
   const cart = await Order.getCart(req.user._id);
+  res.json(cart);
+}
+
+async function addToCart(req, res) {
+  const cart = await Order.getCart(req.user._id);
+  await cart.addToCart(req.body.itemId, req.body.size, req.body.quantity);
   res.json(cart);
 }
