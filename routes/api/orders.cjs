9d151e83bcf44df9bfaff402a0a04d25db859936
Greg Ells <gregells@gmail.com>
2025-04-09 18:09:15 -0400

    Protect orders routes with ensureLoggedIn middleware

--- routes/api/orders.cjs
+++ routes/api/orders.cjs
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ordersCtrl = require('../../controllers/api/orders.cjs');
+const ensureLoggedIn = require('../../config/ensureLoggedIn.cjs');
 
 // All routes start with /api/orders
 
 // GET /api/orders/cart
-router.get('/cart', ordersCtrl.cart);
+router.get('/cart', ensureLoggedIn, ordersCtrl.cart);
 
 module.exports = router;
