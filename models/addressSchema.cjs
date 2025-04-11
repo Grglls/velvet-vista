27db1d989a2d9fde72f15bb47e4f83f55dce760a
Greg Ells <gregells@gmail.com>
2025-04-11 19:39:43 -0400

    Revise addressSchema

--- models/addressSchema.cjs
+++ models/addressSchema.cjs
@@ -1,14 +1,14 @@
 const Schema = require('mongoose').Schema;
 
 const addressSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phone: { type: String, required: true },
   street: { type: String, required: true },
   city: { type: String, required: true },
   state: { type: String, required: true },
-  zip: { type: String, required: true },
+  postcode: { type: String, required: true },
   country: { type: String, required: true },
-  phone: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
 }, {
   timestamps: true
 });
