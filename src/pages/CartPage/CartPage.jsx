f30d087f714106d7ec70a1b894d844584a55f99b
Greg Ells <gregells@gmail.com>
2025-04-11 19:36:27 -0400

    Add Link to CheckoutPage on CartPage

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,4 +1,5 @@
 import OrderSummary from "../../components/OrderSummary/OrderSummary";
+import { Link } from "react-router-dom";
 
 export default function CartPage({ cart }) {
   // Wait until cart is populated on page reload:
@@ -8,6 +9,9 @@ export default function CartPage({ cart }) {
     <>
       <h1>Items in cart: {cart.lineItems.reduce((acc, curr) => acc + curr.quantity, 0)}</h1>
       <OrderSummary order={cart} />
+      <div className="d-flex justify-content-center">
+        <Link to={'/orders/checkout'} className="btn btn-primary mb-3">Proceed to Checkout</Link>
+      </div>
     </>
   );
 }
