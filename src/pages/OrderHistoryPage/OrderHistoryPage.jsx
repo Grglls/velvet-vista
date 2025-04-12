a97c302d5ba092f61c762789887eec6f80b7177a
Greg Ells <gregells@gmail.com>
2025-04-11 20:11:21 -0400

    Add effect to pull previous orders from the database

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,24 @@
-export default function OrderHistoryPage () {
+import { useState, useEffect } from "react";
+import * as ordersAPI from "../../utilities/orders-api";
+
+export default function OrderHistoryPage() {
+  const [orders, setOrders] = useState([]);
+  
+  useEffect(function() {
+    async function getOrders() {
+      const orders = await ordersAPI.getAll();
+      setOrders(orders);
+    }
+    getOrders();
+  }, []);
+  
+  // Wait until orders are populated:
+  if (!orders) return <p>Loading...</p>;
+  
   return (
     <>
-      <h1>OrderHistoryPage</h1>
+      <h1>Previous Orders</h1>
+      <div>orders: {orders.length}</div>
     </>
   );
 }
