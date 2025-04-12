d601123a7c0639f9044cdab7e754e09a2decdf00
Greg Ells <gregells@gmail.com>
2025-04-11 20:12:09 -0400

    Add getAll function to orders-api

--- src/utilities/orders-api.js
+++ src/utilities/orders-api.js
@@ -3,16 +3,21 @@ import sendRequest from "./send-request";
 const BASE_URL = '/api/orders';
 
 // Retrieve the cart (i.e. the unpaid order) for the current user:
-export async function getCart() {
+export function getCart() {
   return sendRequest(`${BASE_URL}/cart`);
 }
 
 // Add an item to the cart:
-export async function addItemToCart(itemId, size, quantity) {
+export function addItemToCart(itemId, size, quantity) {
   return sendRequest(`${BASE_URL}/cart/items`, 'POST', { itemId, size, quantity });
 }
 
 // Checkout the cart (i.e. update the unpaid order's isPaid property to true):
-export async function checkout(address) {
+export function checkout(address) {
   return sendRequest(`${BASE_URL}/cart/checkout`, 'POST', { address });
+}
+
+// Retrieve all orders for the logged in user:
+export function getAll() {
+  return sendRequest(BASE_URL);
 }
