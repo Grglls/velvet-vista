486f9e383c89296ddafbc5f4de2ad5b598416744
Greg Ells <gregells@gmail.com>
2025-05-30 15:25:46 -0400

    Fix indentation

--- src/components/OrderTabulated/OrderTabulated.jsx
+++ src/components/OrderTabulated/OrderTabulated.jsx
@@ -3,24 +3,24 @@ import LineItem from "../LineItem/LineItem";
 export default function OrderTabulated({ order, handleChangeQuantity }) {
   return (
     <table className="table table-striped align-middle">
-        <thead>
-          <tr>
-            <th scope="col">Item</th>
-            <th scope="col">Size</th>
-            <th scope="col">Qty</th>
-            <th scope="col">Price</th>
-            <th scope="col">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {order.lineItems.map((lineItem, index) => (
-            <LineItem key={index} lineItem={lineItem} handleChangeQuantity={handleChangeQuantity} isPaid={order.isPaid} />
-          ))}
-          <tr>
-            <th colSpan="4">Total</th>
-            <th>${order.totalPrice.toFixed(2)}</th>
-          </tr>
-        </tbody>
-      </table>
+      <thead>
+        <tr>
+          <th scope="col">Item</th>
+          <th scope="col">Size</th>
+          <th scope="col">Qty</th>
+          <th scope="col">Price</th>
+          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.lineItems.map((lineItem, index) => (
+          <LineItem key={index} lineItem={lineItem} handleChangeQuantity={handleChangeQuantity} isPaid={order.isPaid} />
+        ))}
+        <tr>
+          <th colSpan="4">Total</th>
+          <th>${order.totalPrice.toFixed(2)}</th>
+        </tr>
+      </tbody>
+    </table>
   );
 }
