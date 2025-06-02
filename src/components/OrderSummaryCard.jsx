959dbdeb4f59e13fd3481bb4a74c2dd3de8a3d1b
Greg Ells <gregells@gmail.com>
2025-06-02 14:56:21 -0400

    Change date ordered on orders to use updatedAt field rather than createdAt field

--- src/components/OrderSummaryCard.jsx
+++ src/components/OrderSummaryCard.jsx
@@ -10,7 +10,7 @@ export default function OrderSummaryCard({ order }) {
         <h5 className="card-title">Order ID: {order.orderId}</h5>
         <p className="card-text">Items: <span className="float-end">{order.totalQuantity}</span></p>
         <p className="card-text">Total: <span className="float-end">${order.totalPrice.toFixed(2)}</span></p>
-        <p className="card-text">Date ordered: <span className="float-end">{new Date(order.createdAt).toLocaleDateString()}</span></p>
+        <p className="card-text">Date ordered: <span className="float-end">{new Date(order.updatedAt).toLocaleDateString()}</span></p>
       </div>
     </Link>
   );
--- src/pages/OrderDetailPage.jsx
+++ src/pages/OrderDetailPage.jsx
@@ -19,8 +19,6 @@ export default function OrderDetailPage() {
   // Wait until order is populated:
   if (!order) return <p>Loading...</p>;
 
-  console.log('OrderDetailPage', order.address);
-
   return (
     <>
       <h1>Order Details</h1>
@@ -29,7 +27,7 @@ export default function OrderDetailPage() {
           <h5 className="card-title">Order ID: {order.orderId}</h5>
           <p className="card-text">Items: <span className="float-end">{order.totalQuantity}</span></p>
           <p className="card-text">Total: <span className="float-end">${order.totalPrice.toFixed(2)}</span></p>
-          <p className="card-text">Date ordered: <span className="float-end">{new Date(order.createdAt).toLocaleDateString()}</span></p>
+          <p className="card-text">Date ordered: <span className="float-end">{new Date(order.updatedAt).toLocaleDateString()}</span></p>
         </div>
       </div>
       <AddressTabulated address={order.address} />
