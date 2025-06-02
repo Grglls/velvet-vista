a6b1cd752dc89b62eec5b50095886a39637d52c1
Greg Ells <gregells@gmail.com>
2025-06-02 16:35:52 -0400

    Use button group for login/signup toggle, following same format on checkout page

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -8,18 +8,25 @@ export default function AuthPage ({ setUser }) {
   return (
     <main className="d-flex flex-column">
       <h1 className="my-3 mx-auto">Log In or Sign Up</h1>
-      <div className="mx-auto">
-        <button
-          onClick={() => setShowLogin(!showLogin)}
-          className="btn btn-secondary mb-3"
+      <div className="btn-group mb-3 container-sm mw-100" style={{width: '400px'}} role="group">
+        <button 
+          className={showLogin ? "btn btn-primary" : "btn btn-outline-primary"}
+          onClick={() => setShowLogin(true)}
         >
-          { showLogin ? 'Sign Up' : 'Log In' }
+          Log In
+        </button>
+        <button 
+          className={!showLogin ? "btn btn-primary" : "btn btn-outline-primary"}
+          onClick={() => setShowLogin(false)}
+        >
+          Sign Up
         </button>
       </div>
+
       { showLogin ? 
-          <LoginForm setUser={setUser} />
-          :
-          <SignUpForm setUser={setUser} />
+        <LoginForm setUser={setUser} />
+        :
+        <SignUpForm setUser={setUser} />
       }
     </main>
   );
