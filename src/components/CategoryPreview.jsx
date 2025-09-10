b40b8fd38995fefbc72fbd8d9d379efc2ade3246
Greg Ells <gregells@gmail.com>
2025-09-09 21:40:16 -0400

    Add text to category links on Home Page to make it more obvious to users

--- src/components/CategoryPreview.jsx
+++ src/components/CategoryPreview.jsx
@@ -4,8 +4,17 @@ import ClothingItemCard from "./ClothingItemCard";
 export default function CategoryPreview({ category, clothes }) {
   return (
     <div className="mb-3">
-      <Link to={`/category/${category}`} className="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" >
-        <h5 className="text-decoration-underline d-inline-block">{category}:</h5>
+      <Link
+        to={`/category/${category}`}
+        className="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+      >
+        <h5 className="d-inline-block mb-0 text-decoration-underline">
+          {category}:
+        </h5>
+        <span className="d-inline-block">&nbsp;&nbsp;</span>
+        <small>
+          view all {category} &rarr;
+        </small>
       </Link>
       <div className="row row-cols-3 p-2">
         {clothes.map((item, index) => index < 3 && <ClothingItemCard item={item} key={index} /> )}
